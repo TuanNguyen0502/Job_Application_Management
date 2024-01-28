35079e5a318256f4968d57793e63028f626a8ca1
Tuan <nguyenhahongtuan@gmail.com>
2024-01-28 23:07:54 +0700

    Create some Form

--- FCandidate.cs
+++ FCandidate.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Job_Application_Management
 {
-    public partial class Form1 : Form
+    public partial class FCandidate : Form
     {
-        public Form1()
+        public FCandidate()
         {
             InitializeComponent();
         }
