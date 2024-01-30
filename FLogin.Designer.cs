94e5209dd47ae068ea0e908ce6d740cae35171f9
Tuan <nguyenhahongtuan@gmail.com>
2024-01-31 00:33:18 +0700

    Done with Login form

--- FLogin.Designer.cs
+++ FLogin.Designer.cs
@@ -28,21 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.label_UserName = new System.Windows.Forms.Label();
+            this.label_Password = new System.Windows.Forms.Label();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.button_ForgerPassword = new System.Windows.Forms.Button();
+            this.button_SignUp = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_UserName.Location = new System.Drawing.Point(382, 138);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(191, 40);
+            this.textBox_UserName.TabIndex = 0;
+            // 
+            // label_UserName
+            // 
+            this.label_UserName.AutoSize = true;
+            this.label_UserName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label_UserName.Font = new System.Drawing.Font("Consolas", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_UserName.Location = new System.Drawing.Point(230, 145);
+            this.label_UserName.Name = "label_UserName";
+            this.label_UserName.Size = new System.Drawing.Size(75, 33);
+            this.label_UserName.TabIndex = 1;
+            this.label_UserName.Text = "User";
+            this.label_UserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_Password
+            // 
+            this.label_Password.AutoSize = true;
+            this.label_Password.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label_Password.Font = new System.Drawing.Font("Consolas", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Password.Location = new System.Drawing.Point(230, 214);
+            this.label_Password.Name = "label_Password";
+            this.label_Password.Size = new System.Drawing.Size(135, 33);
+            this.label_Password.TabIndex = 3;
+            this.label_Password.Text = "Password";
+            this.label_Password.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Password.Location = new System.Drawing.Point(382, 207);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(191, 40);
+            this.textBox_Password.TabIndex = 2;
+            // 
+            // button_Login
+            // 
+            this.button_Login.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Login.Location = new System.Drawing.Point(236, 324);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(129, 53);
+            this.button_Login.TabIndex = 4;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = true;
+            // 
+            // button_ForgerPassword
+            // 
+            this.button_ForgerPassword.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_ForgerPassword.Location = new System.Drawing.Point(354, 407);
+            this.button_ForgerPassword.Name = "button_ForgerPassword";
+            this.button_ForgerPassword.Size = new System.Drawing.Size(219, 42);
+            this.button_ForgerPassword.TabIndex = 5;
+            this.button_ForgerPassword.Text = "Forget password ?";
+            this.button_ForgerPassword.UseVisualStyleBackColor = true;
+            // 
+            // button_SignUp
+            // 
+            this.button_SignUp.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_SignUp.Location = new System.Drawing.Point(395, 324);
+            this.button_SignUp.Name = "button_SignUp";
+            this.button_SignUp.Size = new System.Drawing.Size(178, 53);
+            this.button_SignUp.TabIndex = 6;
+            this.button_SignUp.Text = "Sign Up";
+            this.button_SignUp.UseVisualStyleBackColor = true;
             // 
             // FLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(912, 488);
+            this.ClientSize = new System.Drawing.Size(913, 634);
+            this.Controls.Add(this.button_SignUp);
+            this.Controls.Add(this.button_ForgerPassword);
+            this.Controls.Add(this.button_Login);
+            this.Controls.Add(this.label_Password);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.label_UserName);
+            this.Controls.Add(this.textBox_UserName);
             this.Name = "FLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_UserName;
+        private System.Windows.Forms.Label label_UserName;
+        private System.Windows.Forms.Label label_Password;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Button button_ForgerPassword;
+        private System.Windows.Forms.Button button_SignUp;
     }
 }
 
