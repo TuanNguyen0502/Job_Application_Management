e0cbf38fc93e8d0165b87b675cae0072fa3e0328
Tuan <nguyenhahongtuan@gmail.com>
2024-01-31 23:44:40 +0700

    #

---
+++ FEmployer_FindCandidate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Job_Application_Management
+{
+    public partial class FEmployer_FindCandidate : Form
+    {
+        public FEmployer_FindCandidate()
+        {
+            InitializeComponent();
+        }
+    }
+}
