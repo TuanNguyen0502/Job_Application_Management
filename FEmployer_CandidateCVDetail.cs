7dba841ab63baaf255b7e25ea4b56bdf26cdd97e
Tuan <nguyenhahongtuan@gmail.com>
2024-02-27 22:45:07 +0700

    Rename

--- FEmployer_CandidateCVDetail.cs
+++ FEmployer_CandidateCVDetail.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Job_Application_Management
 {
-    public partial class frmCandCVDetail : Form
+    public partial class FEmployer_CandidateCVDetail : Form
     {
-        public frmCandCVDetail()
+        public FEmployer_CandidateCVDetail()
         {
             InitializeComponent();
         }
