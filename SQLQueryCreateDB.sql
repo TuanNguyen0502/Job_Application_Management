fd103c3594d130407cf6ac350ef278da97f6c66e
Nguyễn Hà Hồng Tuấn <nguyenhahongtuan@gmail.com>
2024-03-18 20:04:02 +0700

    Update SQLQueryCreateDB.sql

--- SQLQueryCreateDB.sql
+++ SQLQueryCreateDB.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE Candidates(
+CREATE TABLE Candidates(
 	CddID varchar(10) CONSTRAINT PK_Cdd PRIMARY KEY,
 	CddName nvarchar(100),
 	Phone varchar(12),
@@ -10,17 +10,20 @@
 )
 
 CREATE TABLE Employeers(
-	EprID varchar(10) CONSTRAINT PK_Epr PRIMARY KEY,
-	EprName nvarchar(100),
+	EmpID varchar(10) CONSTRAINT PK_Epr PRIMARY KEY,
+	EmpEmail varchar(100),
+	EmpName nvarchar(100),
+	Sex nvarchar(10),
 	Phone varchar(12),
 	Workplace nvarchar(250),
-	WorkLocation nvarchar(250),
-	District nvarchar(100)
+	Company nvarchar(250),
+	CompanyAddress nvarchar(250)
 )
 
 CREATE TABLE Jobs(
 	JobID varchar(10) CONSTRAINT PK_Job PRIMARY KEY,
 	JobName nvarchar(250),
+	Salary float(10),
 	CompanyName nvarchar(250),
 	WorkAddress nvarchar(250),
 	JobDecription text,
@@ -29,7 +32,7 @@ CREATE TABLE Jobs(
 	ExpirationDate date,
 	Benefit text,
 	RequestCdd text,
-	EprID varchar(10) CONSTRAINT FK_EprID FOREIGN KEY REFERENCES Employeers(EprID)
+	EmpID varchar(10) CONSTRAINT FK_EmpID FOREIGN KEY REFERENCES Employeers(EmpID)
 )
 
 CREATE TABLE ApplicationDetails(
@@ -39,17 +42,7 @@ CREATE TABLE ApplicationDetails(
 	StateApp nvarchar(100)
 	CONSTRAINT PK_AD PRIMARY KEY (CddID, JobID)
 )
-ALTER TABLE Jobs
-ADD Salary float(10)
-ALTER TABLE Jobs
-DROP CONSTRAINT FK_EprID
-ALTER TABLE Jobs
-ADD CONSTRAINT FK_EprID FOREIGN KEY (EprID) REFERENCES Employeers(EprID)
-ALTER TABLE ApplicationDetails
-add CONSTRAINT FK_Job  FOREIGN KEY (JobID)REFERENCES Jobs(JobID)
 
-TRUNCATE TABLE Candidates
-TRUNCATE TABLE ApplicationDetails
 -- Dữ liệu cho bảng Candidates
 INSERT INTO Candidates (CddID, CddName, Phone, Email, CddAddress, Hometown, Sex, Education)
 VALUES 
@@ -58,16 +51,12 @@ VALUES
 ('CDD003', N'Phạm Văn C', '0123456789', 'phamvanc@example.com', '789 DEF Street', N'Đà Nẵng', N'Nam', 'Bachelor of Arts in English');
 
 -- Dữ liệu cho bảng Employeers
-TRUNCATE TABLE Employeers
-INSERT INTO Employeers (EprID, EprName, Phone, Workplace, WorkLocation, District)
+INSERT INTO Employeers (EmpID, EmpEmail, EmpName, Sex, Phone, Workplace, Company, CompanyAddress)
 VALUES 
-('EPR001', 'Công ty ABC', '9876543210', '123 PQR Road', N'Hà Nội', N'Ba Đình'),
-('EPR002', 'Công ty XYZ', '0123456789', '456 STU Road', N'Hồ Chí Minh City', N'Quận 1'),
-('EPR003', 'Công ty DEF', '0901234567', '789 MNO Road', N'Đà Nẵng', N'Hải Châu');
+('EMP001', 'hongtuan@gmail.com', N'Hồng Tuấn', N'Nam', '0123456789', N'Giám đốc', N'Công ty ABC', N'123 PQR Road, Ba Đình, Hà Nội')
 
 -- Dữ liệu cho bảng Jobs
-TRUNCATE TABLE Jobs
-INSERT INTO Jobs (JobID, JobName, CompanyName, WorkAddress, JobDecription, WorkDuration, Experience, ExpirationDate, Salary, Benefit, RequestCdd, EprID)
+INSERT INTO Jobs (JobID, JobName, CompanyName, WorkAddress, JobDecription, WorkDuration, Experience, ExpirationDate, Salary, Benefit, RequestCdd, EmpID)
 VALUES 
 ('JOB001', 'Software Engineer', 'Công ty ABC', '123 PQR Road, Hanoi', 'Develop and maintain software applications.', 12, '3+ years', '2024-04-30', 2000.5, 'Health insurance, bonus', 'Bachelor of Science in Computer Science, experience with Python and JavaScript preferred', 'EPR001'),
 ('JOB002', 'Marketing Manager', 'Công ty XYZ', '456 STU Road, Ho Chi Minh City', 'Plan and execute marketing campaigns.', 24, '5+ years', '2024-05-15', 3000.9, 'Travel allowance, performance bonus', 'Bachelor’s degree in Marketing or related field, strong leadership skills', 'EPR002'),
