1df1bb1a6daea0b42a483e2fcd50f7648a0ce0be
hieu260904 <hieu01bdvn@gmail.com>
2024-03-27 18:09:30 +0700

    fix: modifier create cv

--- FCandidate_CreateCV.Designer.cs
+++ FCandidate_CreateCV.Designer.cs
@@ -39,15 +39,16 @@
             this.uc_AtFormCreateCV.Location = new System.Drawing.Point(0, 0);
             this.uc_AtFormCreateCV.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.uc_AtFormCreateCV.Name = "uc_AtFormCreateCV";
-            this.uc_AtFormCreateCV.Size = new System.Drawing.Size(1271, 840);
+            this.uc_AtFormCreateCV.Size = new System.Drawing.Size(1282, 1055);
             this.uc_AtFormCreateCV.TabIndex = 0;
+            this.uc_AtFormCreateCV.Load += new System.EventHandler(this.uc_AtFormCreateCV_Load);
             // 
             // FCandidate_CreateCV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1271, 840);
+            this.ClientSize = new System.Drawing.Size(1282, 1055);
             this.Controls.Add(this.uc_AtFormCreateCV);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FCandidate_CreateCV";
--- FCandidate_CreateCV.cs
+++ FCandidate_CreateCV.cs
@@ -22,5 +22,10 @@ namespace Job_Application_Management
             CV cv = new CV(uc_AtFormCreateCV.RichTextBox_CareerObjective, uc_AtFormCreateCV.TextBox_Phone, uc_AtFormCreateCV.TextBox_Email, uc_AtFormCreateCV.TextBox_Address, uc_AtFormCreateCV.RichTextBox_EducationDetail, uc_AtFormCreateCV.TextBox_School, uc_AtFormCreateCV.TextBox_Start, uc_AtFormCreateCV.TextBox_End, uc_AtFormCreateCV.TextBox_Major, uc_AtFormCreateCV.TextBox_Compay, uc_AtFormCreateCV.TextBox_Place, uc_AtFormCreateCV.TextBox_CertificateName, uc_AtFormCreateCV.KryptonDateTimePicker1);
             Console.WriteLine(cv.CareerGoal);
         }
+
+        private void uc_AtFormCreateCV_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
